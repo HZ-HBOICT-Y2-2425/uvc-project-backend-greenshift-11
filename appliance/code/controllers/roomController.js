8e3efb8ad745602c8f2e36c3b16896763a2cf460
zmyslek <zuzia.myslek@gmail.com>
2025-01-09 19:07:46 +0100

    working on it

--- appliance/code/controllers/roomController.js
+++ appliance/code/controllers/roomController.js
@@ -1,10 +1,15 @@
-import { JSONFilePreset } from "lowdb/node";
+import { Low, JSONFile } from 'lowdb';
 
-// Read or create db.json
-// defaultData specifies the structure of the database
-const defaultData = { meta: {"tile": "List of rooms","date": "December 2024"}, rooms : [] }
-const db = await JSONFilePreset('db.json', defaultData)
-const rooms = db.data.rooms
+const file = new JSONFile('db.json');
+const db = new Low(file);
+await db.read(); 
+
+if (!db.data) {
+  db.data = { meta: { "tile": "List of rooms", "date": "December 2024" }, rooms: [] };
+  await db.write();
+}
+
+const rooms = db.data.rooms;
 
 export async function getAllRooms(req, res) {
   const roomUrls = rooms.map(room => `/co2/${room.id}`);
@@ -13,27 +18,24 @@ export async function getAllRooms(req, res) {
 
 export async function updateRoom(req, res) {
   let id = Number(req.params.id);
-  let room = rooms.find(room => room.id === id);
-  if (room) {
+  let roomIndex = rooms.findIndex(room => room.id === id);
+  
+  if (roomIndex !== -1) {
     let name = req.query.name;
     let icon = req.query.icon;
-    let appliances = req.query.appliances;
-    room = { id: id, name: name, icon: icon, appliances: appliances };  
-    // todo remove log
-    console.log(room);
-    rooms.push(room);
+    let appliances = req.query.appliances; // Adjust parsing if needed
+    
+    rooms[roomIndex] = { id, name, icon, appliances };  
     await db.write();
-    res.status(201).send(`I updated this room: ${JSON.stringify(room)}?`);
+    res.status(200).send(`Updated room: ${JSON.stringify(rooms[roomIndex])}`);
   } else {
     res.status(404).send(`The room with id ${id} doesn't exist`);
   }
 }
 
 export async function getRoomById(req, res) {
-  console.log('Database contents:', rooms); // Log all rooms
   const room = rooms.find(a => a.id === parseInt(req.params.id));
-  console.log(`Looking for room with id: ${req.params.id}, found:`, room);
-
+  
   if (room) {
     res.status(200).json(room);
   } else {
@@ -49,9 +51,8 @@ export async function createRoom(req, res) {
   let name = req.query.name;
   let icon = req.query.icon;
   let appliances = req.query.appliances;
-  let room = { id: id, name: name, icon: icon, appliances: appliances };  
-  // todo remove log
-  console.log(room);
+  let room = { id, name, icon, appliances };  
+  
   rooms.push(room);
   await db.write();
   res.status(201).json({ message: "Room created successfully" });
@@ -60,28 +61,23 @@ export async function createRoom(req, res) {
 export async function deleteRoomsByIDs(req, res) {
   let startID = Number(req.params.start);
   let endID = Number(req.params.end);
+  
   if (startID > endID) {
-    res.status(404).send("The end id is smaller or the same as start id");
-  } else {
-    let indexes = [];
-    for (let id = startID; id <= endID; id++) {
-      if (rooms.find(room => room.id === id)) {
-        let i = rooms.indexOf(rooms.find(room => room.id === id));
-        indexes.push(i);
-      } else {
-        console.log(`There is no room with id: ${id}`);
-      }
-    }
-    indexes.forEach(index => rooms.splice(index));
-    let IDsLeft = [];
-    rooms.forEach(room => { IDsLeft.push(room.id) });
+    return res.status(400).send("The end id must be greater than or equal to start id");
+  }
+
+  let initialLength = rooms.length; // Store the initial length to avoid deleting in an iteration issue
+  rooms = rooms.filter(room => room.id < startID || room.id > endID);
+  
+  if (rooms.length < initialLength) {
     await db.write();
-    res.status(200).send(`${IDsLeft}`);
+    res.status(200).send(`Removed rooms with IDs from ${startID} to ${endID}`);
+  } else {
+    res.status(404).send("No rooms were deleted, please check the IDs");
   }
 }
 
 export async function getRoomNames(req, res) {
-  console.log('Rooms:', rooms); // Log rooms
   const ids = rooms.map(room => room.id);
   const icons = rooms.map(room => room.icon);
   const names = rooms.map(room => room.name);
